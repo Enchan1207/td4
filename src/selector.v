9c44e69a64c3186f2d05069d6a75206d94ef6937
Enchan <enchant0130@gmail.com>
2024-10-14 16:51:49 +0900

    [Fix #5] data selector

--- src/selector.v
+++ src/selector.v
@@ -9,16 +9,17 @@ module DataSelector #(
     output [bitWidth-1:0] Y
 );
 
-    function [bitWidth-1:0] select_data;
+    function [bitWidth-1:0] sel;
+        input [bitWidth-1:0] A, B, C, D;
         input [1:0] n;
         case (n)
-            2'd0: select_data = A;
-            2'd1: select_data = B;
-            2'd2: select_data = C;
-            2'd3: select_data = D;
+            2'd0: sel = A;
+            2'd1: sel = B;
+            2'd2: sel = C;
+            2'd3: sel = D;
         endcase
     endfunction
 
-    assign Y = select_data(OE);
-    
+    assign Y = sel(A, B, C, D, OE);
+
 endmodule
